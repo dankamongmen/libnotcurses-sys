8e15fe24db22ac82e61d2454f585612fa51dd802
joseLuís <joseluis@andamira.net>
2021-09-20 15:21:48 +0200

    update `input` example & remove `issue-input-2169`

--- examples/input.rs
+++ examples/input.rs
@@ -1,7 +1,6 @@
 //! Input example
 //!
 //! https://github.com/dankamongmen/notcurses/blob/master/USAGE.md#input
-//!
 
 use libnotcurses_sys::*;
 
@@ -10,16 +9,18 @@ fn main() -> NcResult<()> {
         NCOPTION_SUPPRESS_BANNERS | NCOPTION_NO_WINCH_SIGHANDLER | NCOPTION_NO_QUIT_SIGHANDLERS,
     )?;
 
-    println!("Exit with F1\n");
+    println!("Input example.\nPress any key to continue:");
+    let key = nc.getc_blocking(None)?;
+    println!("Pressed: {}\n", key);
 
+    println!("Press more keys to see their input. You can exit with F1.\n");
     let mut input = NcInput::new_empty();
-
     loop {
         match nc.getc_nblock(Some(&mut input)) {
             Ok(key) => match key {
                 NCKEY_F01 => break,
                 NCKEY_ESC..=NCKEY_BUTTON11 => {
-                    println!("'{0}' ({1:x})\n{2:?}", key, key as u32, input);
+                    println!("'{0}' ({1:x})\n{2:#?}\n", key, key as u32, input);
                 }
                 _ => (),
             },
@@ -27,7 +28,6 @@ fn main() -> NcResult<()> {
         }
     }
 
-    println!("\nExiting...");
     nc.stop()?;
     Ok(())
 }
