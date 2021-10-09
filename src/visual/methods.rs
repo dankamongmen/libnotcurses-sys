fb13886fed44504a933569009fd4467f7a82e1b4
joseLuís <joseluis@andamira.net>
2021-10-09 16:34:14 +0200

    add pxoffy & pxoffx NcVisualOptions fields

--- src/visual/methods.rs
+++ src/visual/methods.rs
@@ -43,6 +43,8 @@ impl NcVisualOptions {
         beg_x: NcDim,
         len_y: NcDim,
         len_x: NcDim,
+        // pxoff_y: NcDim,
+        // pxoff_x: NcDim,
         blitter: NcBlitter,
         flags: u32,
         transcolor: NcRgba,
@@ -65,9 +67,13 @@ impl NcVisualOptions {
             // bitmask over NCVISUAL_OPTION_*
             flags: flags as u64,
             transcolor,
+            // WIP
+            pxoffy: 0,
+            pxoffx: 0,
         }
     }
 
+    // TODO: use Option<> groups for coords
     pub fn without_plane(
         y: NcDim,
         x: NcDim,
@@ -75,6 +81,8 @@ impl NcVisualOptions {
         beg_x: NcDim,
         len_y: NcDim,
         len_x: NcDim,
+        // pxoff_y: NcDim,
+        // pxoff_x: NcDim,
         blitter: NcBlitter,
         flags: u32,
         transcolor: u32,
@@ -82,7 +90,7 @@ impl NcVisualOptions {
         Self {
             n: null_mut(),
             scaling: crate::NCSCALE_NONE,
-            // where the created ncplane will be placed relative to the standard plane's origin
+            // where the created plane will be placed relative to stdplane's origin
             y: y as i32,
             x: x as i32,
             // origin of rendered section
@@ -97,6 +105,17 @@ impl NcVisualOptions {
             flags: flags as u64,
             // This color will be treated as transparent with flag [NCVISUAL_OPTION_ADDALPHA].
             transcolor,
+            // pixel offsets within the cell.
+            // if NCBLIT_PIXEL is used, the bitmap will be drawn offset from the
+            // upper-left cell's origin by these amounts. it is an error if
+            // either number exceeds the cell-pixel geometry in its dimension.
+            // if NCBLIT_PIXEL is not used, these fields are ignored.
+            // this functionality can be used for smooth bitmap movement.
+            // WIP
+            pxoffy: 0,
+            pxoffx: 0,
+            // pxoffy: pxoff_y,
+            // pxoffx: pxoff_x,
         }
     }
 
