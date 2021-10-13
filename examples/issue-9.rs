e6185d3f897ff385f7cbec2cbad50373e7af81aa
joseLuís <joseluis@andamira.net>
2021-10-13 13:22:11 +0200

    update example #9

--- examples/issue-9.rs
+++ examples/issue-9.rs
@@ -1,41 +1,50 @@
-// https://doc.rust-lang.org/std/ffi/struct.CString.html
 //
+// FIX memory leak while giving ownership of strings to C
 
-use core::ptr::null_mut;
-use libnotcurses_sys::{*, widgets::*};
-
-// FIX memleak while using strings
+use libnotcurses_sys::{
+    c_api::libc::{free, strdup},
+    widgets::*,
+    *,
+};
+use std::{ffi::c_void, ptr::null_mut};
 
 fn main() -> NcResult<()> {
-    let nc: &mut Nc = Nc::new()?;
-    let plane = NcPlane::new(nc, 0,0,10,10)?;
+    let nc: &mut Nc = Nc::new_cli()?;
+    let plane = NcPlane::new(nc, 0, 0, 10, 10)?;
 
-    // WIP
-    let option_str = std::ffi::CString::new("X").expect("option_str");
+    // SETUP STRINGS
+    //
+    // <https://doc.rust-lang.org/std/ffi/struct.CString.html>
+    //
+    // 1. generate a C-compatible string from a Rust byte slice.
+    // 2. the `as_ptr` method gives a `*const c_char` to feed functions that
+    //    expect a nul-terminated string, like C’s `strdup`.
+    // 3. `strdup` creates a duplicate of the string that gives a `*mut c_char`
+    //    (that must be freed later).
+    let str_title = std::ffi::CString::new("title").unwrap();
+    let str_title_ptr = str_title.as_ptr();
+    let str_title_dup = unsafe { strdup(str_title_ptr) };
+    //
+    // let str_option = std::ffi::CString::new("item_option").expect("CString::new");
+    // let str_option_ptr = str_option.as_ptr();
+    // let str_option_dup = unsafe { strdup(str_option_ptr)};
 
+    // SETUP SELECTOR
+    //
     let item1 = NcSelectorItem {
-
-        // this is OK:
         option: null_mut(),
-        // this causes memory leak
-        // option: unsafe { c_api::libc::strdup(option_str.as_ptr()) },
-
+        // option: str_option_dup, // <-- ncselector_destroy doesn't free this
         desc: null_mut(),
-        opcolumns:0,
-        desccolumns:0,
+        opcolumns: 0,
+        desccolumns: 0,
     };
-
-    let mut selector_items: [NcSelectorItem; 1] = [
-        item1,
-    ];
-
-
-
+    let mut selector_items: [NcSelectorItem; 1] = [item1];
     let seloptions = NcSelectorOptions {
-        title: null_mut(),
+        // title: null_mut(),
+        title: str_title_dup, // <-- ncselector_destroy doesn't free this
+
         secondary: null_mut(),
         footer: null_mut(),
-
         items: selector_items.as_mut_ptr(),
         defidx: 0,
         maxdisplay: 0,
@@ -46,12 +55,20 @@ fn main() -> NcResult<()> {
         boxchannels: 0,
         flags: 0,
     };
-
     let selector = NcSelector::new(plane, seloptions)?;
 
-    plane.destroy()?;
+    // CLEANUP
 
-    selector.destroy()?;
+    // plane.destroy()?; // NOTE: can't call plane destroy, valgrind complains
+    unsafe {
+        c_api::ncselector_destroy(selector, null_mut());
+    };
+    // plane.destroy()?; // also here!
+
+    // NOTE: It seems I must always free the strings manually to avoid memleaks,
+    // because ncselector_destroy() isn't taking care of it... I don't know why
+    unsafe { free(str_title_dup as *mut c_void) };
+    // unsafe { free(str_option_dup as *mut c_void) };
 
     nc.stop()?;
     Ok(())
