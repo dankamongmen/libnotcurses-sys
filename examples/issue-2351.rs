2a0c2eaee723ce1d5606a606d6fc15fe53041fc3
joseLuís <joseluis@andamira.net>
2021-11-17 20:58:52 +0100

    add new example for nc issue 2351

---
+++ examples/issue-2351.rs
@@ -0,0 +1,44 @@
+// found issues relative to visual geometry
+
+use libnotcurses_sys::*;
+use std::ptr::null_mut;
+
+fn main() -> NcResult<()> {
+    let nc = Nc::new_cli()?;
+
+    let stdp = nc.stdplane();
+    stdp.set_scrolling(true);
+
+    // # ISSUE 1
+    // When providing NcBlitter::DEFAULT it always picks NcBlitter::HALF
+    // even if NcBlitter::QUADRANT is supported
+
+    let vo = NcVisualOptions::without_plane(0, 0, 0, 0, 0, 0, NcBlitter::DEFAULT, 0, 0);
+
+    // let vg = nc.visual_geom(None, Some(&vo))?;
+    let mut vg = NcVGeom::new();
+    unsafe { crate::c_api::ncvisual_geom(nc, null_mut(), &vo, &mut vg) };
+
+    putstrln!(stdp, "{:?}\n", vg)?;
+
+    let (h, w) = stdp.dim_yx();
+    let (y, x) = (h * vg.scaley, w * vg.scalex);
+    let buffer: Vec<u8> = vec![200; (y * x) as usize * 3];
+
+    let vo = NcVisualOptions::without_plane(0, 0, 0, 0, 0, 0, NcBlitter::PIXEL, 0, 0);
+    let visual = NcVisual::from_rgb_packed(buffer.as_slice(), y, x * 3, x, 255)?;
+
+    // # ISSUE 2
+    // `NcVGeom` created from `ncvisual_geom` is missing `maxpixely`:
+
+    // let vgeom = nc.visual_geom(Some(visual), Some(&vo))?;
+    // putstrln!(stdp, "{:?}\n{:?}", vgeom, vgeom.blitter_name())?;
+    let mut vg = NcVGeom::new();
+    unsafe { crate::c_api::ncvisual_geom(nc, visual, &vo, &mut vg) };
+
+    putstrln!(stdp, "{:?}", vg)?;
+
+    visual.destroy();
+    nc.stop()?;
+    Ok(())
+}
