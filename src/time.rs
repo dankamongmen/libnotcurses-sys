3e709b6c60a2eb0637578636d410fe2cff382132
joseLuís <joseluis@andamira.net>
2023-04-13 13:51:57 +0200

    fix `NcTime` to use `libc` when available

--- src/time.rs
+++ src/time.rs
@@ -1,6 +1,13 @@
 //! `NcTime`
 
-use crate::c_api::ffi::{__syscall_slong_t, __time_t, timespec};
+use crate::c_api::ffi::timespec;
+
+#[cfg(not(feature = "libc"))]
+use crate::c_api::ffi::{__syscall_slong_t as c_long, __time_t as time_t};
+
+// needed for MacOs
+#[cfg(feature = "libc")]
+use libc::{c_long, time_t};
 
 /// A time in seconds and nanoseconds.
 ///
@@ -13,9 +20,7 @@ pub type NcTime = timespec;
 
 impl NcTime {
     /// New `NcTime` with the specified seconds and nanoseconds.
-    //
-    // Both fields should be i64 on 64-bit platforms, and i32 on 32-bit ones.
-    pub fn new(seconds: __time_t, nanoseconds: __syscall_slong_t) -> Self {
+    pub fn new(seconds: time_t, nanoseconds: c_long) -> Self {
         Self { tv_sec: seconds, tv_nsec: nanoseconds }
     }
 }
